8ca9f1ee65574b6aafb10ac4eae5da2d042e5c3b
İbraihm Şimşek <ibrahimsimsek590@gmail.com>
2023-07-15 01:09:39 +0300

    unit test projesi eklendi ve getpersonbyid metotunun testi yazıldı

---
+++ PhoneDictionary/PhoneDictionary.Test/PersonTest.cs
@@ -0,0 +1,39 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using AutoFixture.Xunit2;
+using Castle.Core.Resource;
+using FluentAssertions;
+using PhoneDictionary._Business.Services;
+using PhoneDictionary.Entity;
+using PhoneDictionary.Entity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace PhoneDictionary.Test
+{
+    public class PersonTest
+    {
+
+        private IPhoneDictionaryService _service;
+        public PersonTest()
+        {
+            _service = new PhoneDictionaryService();
+        }
+
+        [Fact]
+        public void PhoneDictionaryService_GetPersonIdWithInvalidArguments_ThrowsException()
+        {
+            //arrange
+            int id = default(int);
+            // act & assert
+            Assert.Throws<Exception>(() => _service.GetPersonById(id));
+        }
+
+    }
+
+  
+}
