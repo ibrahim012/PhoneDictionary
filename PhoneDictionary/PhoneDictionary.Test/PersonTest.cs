ae75a19d55739df460de3f784e17df822b98c46f
İbraihm Şimşek <ibrahimsimsek590@gmail.com>
2023-07-16 23:28:18 +0300

    test metotları yazıldı

--- PhoneDictionary/PhoneDictionary.Test/PersonTest.cs
+++ PhoneDictionary/PhoneDictionary.Test/PersonTest.cs
@@ -3,7 +3,9 @@ using AutoFixture.AutoMoq;
 using AutoFixture.Xunit2;
 using Castle.Core.Resource;
 using FluentAssertions;
+using Moq;
 using PhoneDictionary._Business.Services;
+using PhoneDictionary.DataAccess.Repository;
 using PhoneDictionary.Entity;
 using PhoneDictionary.Entity.Models;
 using System;
@@ -31,6 +33,28 @@ namespace PhoneDictionary.Test
             int id = default(int);
             // act & assert
             Assert.Throws<Exception>(() => _service.GetPersonById(id));
+        }
+
+        [Fact]
+        public void PhoneDictionaryService_DeleteInvalidArguments_ThrowsException()
+        {
+            //arrange
+            int id = default(int);
+            // act & assert
+            Assert.Throws<Exception>(() => _service.DeletePerson(id));
+        }
+
+        [Fact]
+        public void GetPersons_ShouldReturnLists()
+        {
+            // Arrange
+            var service = new PhoneDictionaryService();
+
+            // Act
+            var contacts = service.GetAllPerson();
+
+            // Assert
+            Assert.NotNull(contacts);
         }
 
     }
