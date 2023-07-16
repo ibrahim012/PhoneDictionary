0621829c312ac3c4a95ec0839f46fc29640d631d
İbraihm Şimşek <ibrahimsimsek590@gmail.com>
2023-07-17 01:03:33 +0300

    rbmq rapor tamamlandı

---
+++ PhoneDictionary/PhoneDictionary.API/RabbitMQPublisher.cs
@@ -0,0 +1,25 @@
+﻿using PhoneDictionary.RabbitMQService;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace PhoneDictionary.API
+{
+    public static class RabbitMQPublisher
+    {
+        public static void Publisher(string queueName, string message)
+        {
+            var _rabbitmqClient = new RabbitMQClient();
+
+            using (var connection = _rabbitmqClient.GetRabbitMQConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queueName, false, false, false, null);
+
+                    channel.BasicPublish("", queueName, null, Encoding.UTF8.GetBytes(message));
+                }
+            }
+        }
+    }
+    
+}
---
+++ PhoneDictionary/PhoneDictionary.RabbitMQService/RabbitMQClient.cs
@@ -0,0 +1,19 @@
+﻿using RabbitMQ.Client;
+
+namespace PhoneDictionary.RabbitMQService
+{
+    public class RabbitMQClient
+    {
+        private readonly string _hostName = "localhost";
+
+        public IConnection GetRabbitMQConnection()
+        {
+            ConnectionFactory connectionFactory = new ConnectionFactory()
+            {
+                HostName = _hostName
+            };
+
+            return connectionFactory.CreateConnection();
+        }
+    }
+}
